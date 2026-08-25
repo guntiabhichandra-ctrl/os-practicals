979af1572c7edf9a8852019a668ea48d575944d3
abhi <guntiabhichandra@gmail.com>
2026-08-25 18:10:34 +0530

    Update 2nd.c

---
+++ 2nd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 25;
+    int b = 18;
+
+    if (a > b)
+        printf("%d is the largest.\n", a);
+    else
+        printf("%d is the largest.\n", b);
+
+    return 0;
+}
