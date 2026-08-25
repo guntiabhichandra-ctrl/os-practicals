89d5ebe5c4cbaba7261c15a92542c574fc8c536f
abhi <guntiabhichandra@gmail.com>
2026-08-25 18:15:00 +0530

    Update random1st.c

---
+++ random1st.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 10;
+    int num2 = 20;
+    int sum;
+
+    sum = num1 + num2;
+
+    printf("First number = %d\n", num1);
+    printf("Second number = %d\n", num2);
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
